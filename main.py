c90d42e910e53dd712fa57c56c73480886cf20fe
Mohamed Karoui <mfkaroui@gmail.com>
2019-01-28 13:32:00 -0500

    Added age classes and code to import and normalize images

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import psutil as psu
 from multiprocessing import pool as mpp
 import PIL
 
-imagesPath = "Data/UTKFace/"
-labelsPath = "Data/"
+imagesPath = "Data/UTKFace"
+labelsPath = "Data"
 
 workingDir = os.getcwd()
 imagesPath = os.path.join(workingDir, imagesPath)
@@ -14,6 +14,16 @@ labelsPath = os.path.join(workingDir, labelsPath)
 
 genderClassLabels = ["Male", "Female"]
 raceClassLabels = ["White", "Black", "Asian", "Indian", "Others"]
+ageClassLabels = [(0, 5), (6, 11), (12, 17),
+                  (18, 23), (24, 29), (30, 35),
+                  (36, 41), (42, 47), (48, 53),
+                  (54, 59), (60, 65), (66, 71),
+                  (72, 77), (78, 83), (84, 89),
+                  (90, 95), (96, 101), (102, 107),
+                  (108, 113), (114, 119)]
+
+def readImage(path):
+    return np.asarray(PIL.Image.open(path))
 
 if __name__ == "__main__":
     print("Face Detect\nAuthor: Mohamed Fateh Karoui")
@@ -31,12 +41,27 @@ if __name__ == "__main__":
                 for line in lines:
                     parts = line.split(" ")
                     filenameParts = parts[0].split("_")
-                    if len(filenameParts) == 4:
+                    fullPath = os.path.join(imagesPath, parts[0] + ".chip.jpg")
+                    if len(filenameParts) == 4 and os.path.exists(fullPath):
                         try:
                             age = int(filenameParts[0])
                             gender = int(filenameParts[1])
                             race = int(filenameParts[2])
-                            labels[parts[0]] = (age, gender, race)
+                            labels[fullPath] = (age, gender, race)
                         except:
                             pass
     print("Found " + str(len(labels)) + " labels.")
+
+    images = []
+    for path in list(labels.keys()):
+        images.append(readImage(path))
+
+    images = np.array(images, dtype=float)
+
+    print("Loaded Images. Shape: " + str(images.shape))
+
+    inputImages = images / 255
+    print("Normalized Input Image Data")
+
+    outputAges = np.array([label[0] for label in list(labels.values())], dtype=int)
+    
