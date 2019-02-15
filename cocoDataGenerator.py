21468dd67fa6c1e1cafc861a03a81ebb20e7d535
Mohamed Karoui <mfkaroui@gmail.com>
2019-02-15 00:21:52 -0500

    added region prop nn

---
+++ cocoDataGenerator.py
@@ -0,0 +1,176 @@
+import numpy as np
+import tensorflow as tf
+import random
+
+from PIL import Image, ImageDraw
+from pycocotools.coco import COCO
+import os
+import psutil as psu
+import urllib.request
+import shutil
+import zipfile
+import itertools
+import threading
+
+shape = (256,128)
+
+def getImg(url, result, index):
+    global shape
+    try:
+        with urllib.request.urlopen(url, timeout=3) as resp:
+            img = Image.open(resp.fp)
+            i = img.resize(shape, Image.ANTIALIAS)
+            result[index] = np.asarray(i, dtype=float) / 255
+            if len(result[index].shape) != 3 or result[index].shape[-1] != 3:
+                result[index] = None
+            i.close()
+            img.close()
+    except:
+        result[index] = None
+    return 0
+
+def getLbl(annotation, width, height, personID, result, index):
+    if result[index] is None:
+        img = Image.new("RGB", (width, height), "black")
+    if annotation["category_id"] == personID:
+        draw = ImageDraw.Draw(img)
+        if annotation["iscrowd"] == 1:
+            draw.rectangle([(annotation["bbox"][0], annotation["bbox"][1]), (annotation["bbox"][0] + annotation["bbox"][2], annotation["bbox"][1] + annotation["bbox"][3])], fill=(255,255,255))
+        else:
+            for segment in annotation["segmentation"]:
+                xy = []
+                for i in range(1, len(segment), 2):
+                    xy.append((segment[i - 1], segment[i]))
+                draw.polygon(xy, fill=(255,255,255))
+    i = img.resize(shape, Image.ANTIALIAS)
+    result[index] = np.ceil(np.asarray(i, dtype=float)[:,:,0] / 255)
+    i.close()
+    img.close()
+    return 0
+'''
+The data generator will be responsible for subdividing the input data into training sets and test sets
+It will subsample classes per batch to solve class imbalance issues.
+'''
+class DataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, annotationFile, validationSplit=0.3, samplesPerClass=50, batchCount=10, isValidation = False, validationPerson = None, validationBackground = None):
+        
+        #store the samplesPerClass
+        self.samplesPerClass = samplesPerClass
+        self.batchCount = batchCount
+        #initialize coco api
+        self.coco = COCO(annotationFile)
+        self.isValidation = isValidation
+        person = self.coco.getCatIds(catNms=['person'])
+        self.personID = person[0]
+        if isValidation == False:
+            cats = self.coco.loadCats(self.coco.getCatIds())
+            nms=[cat['name'] for cat in cats]
+            nms.remove("person")
+            
+            self.person = self.coco.getImgIds(catIds=person)
+            background = self.coco.getCatIds(catNms=nms)
+            backgrounds = itertools.combinations(background, 3)
+            self.background = []
+            for b in backgrounds:
+                self.background.extend(self.coco.getImgIds(catIds=b))
+            for p in self.person:
+                try:
+                    self.background.remove(p)
+                except:
+                    pass
+            #initial randomize
+            self.person = np.array(self.person, dtype=int)
+            self.background = np.array(self.background, dtype=int)
+
+            i = random.sample(range(len(self.person)), len(self.person))
+            self.person = self.person[i]
+            personValidationIndex = int(self.person.shape[0] * (1 - validationSplit))
+            personVal = self.person[personValidationIndex:]
+            self.person = self.person[:personValidationIndex]
+            i = random.sample(range(len(self.background)), len(self.background))
+            self.background = self.background[i]
+            backgroundValidationIndex = int(self.background.shape[0] * (1 - validationSplit))
+            backgroundVal = self.background[backgroundValidationIndex:]
+            self.background = self.background[:backgroundValidationIndex]
+            self.validationGenerator = DataGenerator(annotationFile, validationSplit, samplesPerClass, batchCount, True, personVal, backgroundVal)
+            self.sampleData()
+        else:
+            self.person = validationPerson
+            self.background = validationBackground
+
+    def sampleData(self):
+        global shape
+        self.images = []
+        self.labels = []
+        u = []
+        personsIndex = random.sample(range(self.person.shape[0]), self.samplesPerClass)
+        u.extend(self.person[personsIndex])
+        backgroundIndex = random.sample(range(self.background.shape[0]), self.samplesPerClass)
+        u.extend(self.background[backgroundIndex])
+        
+        u = self.coco.loadImgs(u)
+        r = [None] * len(u)
+        threads = [threading.Thread(target=getImg, args=(url["coco_url"],r,u.index(url))) for url in u]
+        for thread in threads:
+            thread.start()
+        while True:
+            recheck = False
+            for thread in threads:
+                if thread.isAlive():
+                    recheck = True
+            if recheck == False:
+                break
+        i = 0
+        totalPersonPixels = 0
+        for res in r:
+            if res is not None:
+                self.images.append(res)
+                a = self.coco.getAnnIds(imgIds=[u[i]["id"]], iscrowd=None)
+                a = self.coco.loadAnns(a)
+                r1 = [None] * len(a)
+                threads = [threading.Thread(target=getLbl, args=(annotation, u[i]["width"], u[i]["height"], self.personID, r1, a.index(annotation))) for annotation in a]
+                for thread in threads:
+                    thread.start()
+                while True:
+                    recheck = False 
+                    for thread in threads:
+                        if thread.isAlive():
+                            recheck = True
+                    if recheck == False:
+                        break
+                r1 = np.stack(r1, axis=0)
+                label = np.amax(r1, axis=0)
+                totalPersonPixels += np.sum(label)
+                label = np.stack([label, 1 - label], axis=2)
+                self.labels.append(label)
+            i = i + 1
+        personWeight = (shape[0] * shape[1] * len(self.images)) / totalPersonPixels
+        self.classWeights = [personWeight, 1 / personWeight]
+        self.images = np.stack(self.images, axis=0)
+        self.labels = np.stack(self.labels, axis=0)
+         
+        #final shuffle
+        indexes = random.sample(range(self.images.shape[0]), self.images.shape[0])
+        self.images = self.images[indexes]
+        self.labels = self.labels[indexes]
+
+        self.len = int(self.images.shape[0] / self.batchCount)
+
+        if self.isValidation == False:
+            self.validationGenerator.sampleData()
+
+    def fit(self, model, epochs, callbacks=[]):
+        model.fit_generator(generator=self, validation_data=self.validationGenerator, class_weight=self.classWeights, epochs=epochs, callbacks=callbacks)
+
+    def __len__(self):
+        return self.batchCount
+
+    def __getitem__(self, index):
+        s = slice(index * self.len, (index + 1) * self.len)
+        return self.images[s], self.labels[s]
+    
+    def on_epoch_end(self):
+        #when we finish an epoch we will shuffle
+        indexes = random.sample(range(self.images.shape[0]), self.images.shape[0])
+        self.images = self.images[indexes]
+        self.labels = self.labels[indexes]
---
+++ regionProp.py
@@ -0,0 +1,104 @@
+import cv2 as cv
+import numpy as np
+import os
+import psutil as psu
+import functools
+import tensorflow as tf
+
+from cocoDataGenerator import DataGenerator
+from cocoDataGenerator import shape as inputShape
+import urllib.request
+import shutil
+import zipfile
+
+import cv2 as cv
+
+dataDir = "COCOData"
+workingDir = os.getcwd()
+dataDir = os.path.join(workingDir, dataDir)
+
+dataType = 'val2017'
+annDir = os.path.join(dataDir, "annotations")
+annZipFile = os.path.join(dataDir, "annotations_train{}.zip".format(dataType))
+annFile = os.path.join(annDir, "instances_{}.json".format(dataType))
+annURL = "http://images.cocodataset.org/annotations/annotations_train{}.zip".format(dataType)
+
+if __name__ == "__main__":
+    print("Region Proposal\nAuthor: Mohamed Fateh Karoui")
+    print("Working Directory : " + workingDir)
+    ncpu = psu.cpu_count()
+    print("Logical CPU's Found : " + str(ncpu))
+    print("Looking for labels data...")
+    if not os.path.exists(annDir):
+        os.makedirs(annDir)
+    if not os.path.exists(annFile):
+        if not os.path.exists(annZipFile):
+            print ("Downloading zipped annotations to " + annZipFile + " ...")
+            with urllib.request.urlopen(annURL) as resp, open(annZipFile, 'wb') as out:
+                shutil.copyfileobj(resp, out)
+            print ("... done downloading.")
+        print ("Unzipping " + annZipFile)
+        with zipfile.ZipFile(annZipFile,"r") as zip_ref:
+            zip_ref.extractall(dataDir)
+        print ("... done unzipping")
+    print ("Will use annotations in " + annFile)
+    data = DataGenerator(annFile)
+
+    ConvTemplate = functools.partial(tf.keras.layers.Conv2D, padding="same", data_format="channels_last", bias_initializer=tf.keras.initializers.lecun_normal(), kernel_initializer=tf.keras.initializers.lecun_normal(), activation=tf.keras.activations.selu)
+    ConvTemplate.__name__ = 'ConvTemplate'
+    TConvTemplate = functools.partial(tf.keras.layers.Conv2DTranspose, padding="same", data_format="channels_last", bias_initializer=tf.keras.initializers.lecun_normal(), kernel_initializer=tf.keras.initializers.lecun_normal(), activation=tf.keras.activations.selu)
+    TConvTemplate.__name__ = 'TConvTemplate'
+    def skipStep(layer_1, layer_2):
+        return tf.keras.layers.Concatenate()([layer_1, layer_2])
+
+    layers = [tf.keras.layers.Input(shape=(inputShape[1], inputShape[0], 3))]
+    dropoutRate = 0.4
+    filterCount = 50
+    kSize = 5
+
+    kernelSizes = []
+    filters = []
+    dropoutRates = []
+    for i in range(1, 5):
+        kernelSizes.append((kSize, kSize))
+        filters.append(filterCount)
+        dropoutRates.append(dropoutRate)
+        l = ConvTemplate(filters=filterCount, kernel_size=(kSize, kSize), strides=(2,2))(layers[i - 1])
+        l = tf.keras.layers.SpatialDropout2D(dropoutRate, "channels_last")(l)
+        dropoutRate = dropoutRate / 2
+        filterCount = int(filterCount / 2)
+        filterCount = 2 if filterCount < 2 else filterCount
+        layers.append(l)
+        kSize = int(kSize * 2)
+        pShape = tf.keras.backend.int_shape(l)
+        mSize = int(min(pShape[1], pShape[2]) / 2)
+        kSize = kSize if kSize < mSize else mSize
+        kSize = kSize - 1 if kSize % 2 == 0 else kSize
+    #inverse operation with skipSteps
+    for i in range(len(filters), 0, -1):
+        l = TConvTemplate(filters=filters[i - 1], kernel_size=kernelSizes[i - 1], strides=(2,2))(layers[-1])
+        l = skipStep(l, layers[i - 1])
+        l = tf.keras.layers.SpatialDropout2D(min(dropoutRates[i - 1] * 2, 0.5), "channels_last")(l)
+        layers.append(l)
+    classification = TConvTemplate(filters=2, kernel_size=(7,7), activation=tf.keras.activations.softmax)(layers[-1])
+    model = tf.keras.models.Model(inputs=layers[0], outputs=classification)
+    model.compile(tf.keras.optimizers.Adam(1e-6),loss=tf.keras.losses.categorical_crossentropy, metrics=["accuracy"])
+
+    def pr(b, l):
+        preview = []
+        images, labels = data.validationGenerator[0]
+        n = 3
+        predictions = model.predict_on_batch(images[:n])
+        dummyChannel = np.zeros((inputShape[1], inputShape[0], 1))
+        for j in range(n):
+            row = [images[j], np.concatenate([labels[j], dummyChannel], axis=-1), np.concatenate([predictions[j], dummyChannel], axis=-1)]
+            preview.append(np.concatenate(row, axis=-2))
+        preview = np.concatenate(preview, axis=-3)
+        cv.imshow("Preview", (preview * 255).astype(np.uint8))
+        cv.waitKey(1)
+    previewResults = tf.keras.callbacks.LambdaCallback(on_batch_begin=pr)
+
+    for i in range(1000):
+        data.fit(model, 10, [previewResults])
+        data.sampleData()
+        
